79b80190026175e60ea91acf472e4ddd0bf4d1eb
LucasPadovan <zerobba8@hotmail.com>
2019-05-05 01:14:10 -0300

    Re render of same route prevented

--- client/Container.jsx
+++ client/Container.jsx
@@ -21,11 +21,11 @@ import Login from '../imports/ui/Accounts/Login';
 
 import './Container.scss';
 
-
+// Guides say to use location.key here but that triggers a re-render of the same component. Using location.pathname prevents this completely.
 const Container = ({ location }) => (
     <TransitionGroup className="body-content">
         <CSSTransition
-            key={location.key}
+            key={location.pathname}
             timeout={{ enter: 250, exit: 250 }}
         >
             <Switch location={location}>
